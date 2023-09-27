50018d4a2efef21ceabc80d23f7f620d55bf8782
abidkhan263187 <abidkhan263187@gmail.com>
2023-09-27 14:57:15 +0530

    add

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const express=require('express')
+const express = require('express')
+var cors = require('cors')
 const { connect } = require('./config.js/db')
 const { formmodel } = require('./Model/formModel')
 
 
 
-const app=express()
+const app = express()
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json())
 
-app.get('/userform', async (req,res)=>{
-    const {userName}=req.body
+app.post('/userform', async (req, res) => {
+    const { userName } = req.body
+
     try {
-        const user= await formmodel.find({userName:userName}) //getting user existing form
+        const user = await formmodel.findOne({ userName: userName }) //getting user existing form
         console.log(user)
-        res.status(200).json({status: 'success',user})
+        res.status(200).json({ status: 'success', user })
     } catch (error) {
         res.status(500).json({ error: "error" })
     }
-  
+
 })
 
 
-app.post('/formSubmit', async(req,res)=>{
-    const{userName,email,phNo,name,dob}=req.body
+app.post('/formSubmit', async (req, res) => {
+    const { userName, email, phNo, name, dob } = req.body
     try {
-       const user= formmodel({
+        const user = formmodel({
             userName: userName,
             email: email,
             phNo: phNo,
@@ -32,28 +37,40 @@ app.post('/formSubmit', async(req,res)=>{
         })
 
         await user.save()
-        res.status(200).json({mssg: "form submitted",user})
+        res.status(200).json({ mssg: "form submitted", user })
     } catch (error) {
-        res.status(500).json({ error: "error" })
-        console.log("error while submitting form",error)
+        res.status(500).json({ error: "error while submitting form" })
+        console.log("error while submitting form", error)
     }
 })
 
-app.put('/updateform',async(req,res)=>{
+app.put('/formUpdate/:id', async (req, res) => {
+    const { id } = req.params;
+    const { userName, email, phNo, name, dob } = req.body;
 
     try {
-        
+        // Using here  findByIdAndUpdate to update the document with the provided id
+        await formmodel.findByIdAndUpdate(id, {
+            userName: userName,
+            email: email,
+            phNo: phNo,
+            name: name,
+            dob: dob,
+        });
+
+        res.status(200).json({ status: true, message: "Form updated successfully" });
     } catch (error) {
-        
+        console.log("Error while updating form ", error);
+        res.status(500).json({ error: "Error while updating form" });
     }
-})
+});
 
 
 
-app.listen(8000,async()=>{
+app.listen(8000, async () => {
     try {
-       await connect
-       console.log("connection established")
+        await connect
+        console.log("connection established")
         console.log("listening on port 8000...")
     } catch (error) {
         console.log("error connecting")
